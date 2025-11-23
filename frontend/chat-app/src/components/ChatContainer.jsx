cfb07bf9fc8f42d964336d1c1b46d31f4119081f
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-11-23 22:14:51 +0530

    feat: add MessageSkeleton component for loading state and enhance ChatHeader with user info and close button

--- frontend/chat-app/src/components/ChatContainer.jsx
+++ frontend/chat-app/src/components/ChatContainer.jsx
@@ -3,6 +3,7 @@ import { useChatStore } from '../store/useChatStore';
 import { useEffect } from 'react';
 import ChatHeader from './ChatHeader';
 import MessageInput from './MessageInput';
+import MessageSkeleton from './skeletons/MessageSkeleton';
 
 const ChatContainer = () => {
   const{messages,getMessages, isMessagesLoading, selectedUser}= useChatStore();
@@ -10,6 +11,14 @@ const ChatContainer = () => {
   useEffect(()=>{
     getMessages(selectedUser._id);
   },[getMessages, selectedUser._id]);
+  if(isMessagesLoading){
+    return <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <MessageSkeleton />
+      <MessageInput />
+     
+    </div>
+  }
 
   return (
    <div className="flex-1 flex flex-col overflow-auto">
