89b3bc7cf4236e910ea1a0baf8a0447b10a4cfa7
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-11-28 21:33:27 +0530

    feat: integrate Socket.IO for real-time communication and update server initialization

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,10 @@ import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import {app,server} from './lib/socket.js';
 
-const app = express();
+
+
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
@@ -22,7 +24,7 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log('Server is running on port 5001');
     connectDB();
 })
