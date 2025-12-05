d59008d6b941f049a3768ca0f5cc3912d2d3ff60
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-12-05 21:32:47 +0530

    just a change

--- frontend/chat-app/src/store/useAuthStore.js
+++ frontend/chat-app/src/store/useAuthStore.js
@@ -85,7 +85,7 @@ export const useAuthStore=create((set,get)=>({
 
         } catch(err){
             console.log(err);
-            toast.error(err.response?.data?.message || "Something went wrong at updating profile");
+            toast.error(err.response?.data?.message || "Something went wrong at updating profile in chatstore");
         } finally{
             set({isUpdatingProfile:false});
         }
