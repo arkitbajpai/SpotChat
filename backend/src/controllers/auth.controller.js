c9dcee21919e52449ced28958a5e689ced18b2a0
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-05-02 23:11:36 +0530

    just a change

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -110,7 +110,7 @@ export const checkAuth=async(req,res)=>{
    try{
    const user = await User.findById(req.user._id).select("-password");
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: "User not found sorry!"  });
     }
     
 
