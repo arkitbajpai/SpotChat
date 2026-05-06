d1ae501301a27fb7521f11f67eb13ca1a4ca7b5a
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-05-06 16:50:54 +0530

    `Update frontend path in production mode`

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,10 +30,10 @@ app.use("/api/messages",messageRoutes)
 app.use("/api/rooms",roomroutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/chat-app/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/chat-app", "dist", "index.html"));
   });
 }
 
