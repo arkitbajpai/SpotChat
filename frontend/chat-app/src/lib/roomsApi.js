e823df9361936a7a282e4f95288777894c5fbfb2
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-05-07 21:27:31 +0530

    fixed the room api

--- frontend/chat-app/src/lib/roomsApi.js
+++ frontend/chat-app/src/lib/roomsApi.js
@@ -1,5 +1,7 @@
-const BASE_URL = "http://localhost:5001";
-
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : "";
 // =======================
 // FETCH NEARBY ROOMS
 // =======================
